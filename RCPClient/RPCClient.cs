c01328bbd6a9e4cfe8497d934c32df189c383829
Devlin <webmasterdevlin@gmail.com>
2019-07-03 09:58:56 +0200

    Add project files.

---
+++ RCPClient/RPCClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new RemoteProcedureCallClient();
+
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
+
+            Console.WriteLine($" [.] Got '{response}'");
+            rpcClient.Close();
+        }
+    }
+}
---
+++ RabbitMQDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
